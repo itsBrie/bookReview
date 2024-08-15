638b5de83e49f5151897ea5a4819df46f6ec483b
Sagwa <121776718+itsBrie@users.noreply.github.com>
2024-08-14 22:47:45 -0500

    Added book, review and user classes to the model file

---
+++ src/main/java/org/example/bookreview/model/User.java
@@ -0,0 +1,22 @@
+package org.example.bookreview.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+}
